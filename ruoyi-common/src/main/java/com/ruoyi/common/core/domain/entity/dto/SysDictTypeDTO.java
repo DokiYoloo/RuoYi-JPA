2dc3c5e7964581e9b38e8d3485a269f23f26e83a
DokiYoloo <43569371+DokiYoloo@users.noreply.github.com>
2023-04-13 11:36:48 +0800

    -

---
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/domain/entity/dto/SysDictTypeDTO.java
@@ -0,0 +1,52 @@
+package com.ruoyi.common.core.domain.entity.dto;
+
+import com.ruoyi.common.annotation.Excel;
+import com.ruoyi.common.annotation.Excel.ColumnType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+/**
+ * 字典类型DTO
+ *
+ * @author DokiYolo
+ */
+@Getter
+@Setter
+@ToString
+public class SysDictTypeDTO {
+
+    /**
+     * 字典主键
+     */
+    @Excel(name = "字典主键", cellType = ColumnType.NUMERIC)
+    private Long dictId;
+
+    /**
+     * 字典名称
+     */
+    @NotBlank(message = "字典名称不能为空")
+    @Size(max = 100, message = "字典类型名称长度不能超过{max}个字符")
+    @Excel(name = "字典名称")
+    private String dictName;
+
+    /**
+     * 字典类型
+     */
+    @NotBlank(message = "字典类型不能为空")
+    @Size(max = 100, message = "字典类型类型长度不能超过{max}个字符")
+    @Pattern(regexp = "^[a-z][a-z0-9_]*$", message = "字典类型必须以字母开头，且只能为（小写字母，数字，下滑线）")
+    @Excel(name = "字典类型")
+    private String dictType;
+
+    /**
+     * 状态（0正常 1停用）
+     */
+    @Excel(name = "状态", readConverterExp = "0=正常,1=停用")
+    private String status;
+
+}
